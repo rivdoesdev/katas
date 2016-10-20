e5df4cc79356f8d754f395573f3fccaed23c7a18
Kate Walters <kawalters01@gmail.com>
2016-10-20 14:57:41 -0500

    Refactored solution

--- raindrops.rb
+++ raindrops.rb
@@ -1,23 +1,35 @@
-def self.convert(number)
-  seven = (number % 7).zero?
-  five = (number % 5).zero?
-  three = (number % 3).zero?
-
-  if seven && five && three
-    return 'PlingPlangPlong'
-  elsif five && three
-    return 'PlingPlang'
-  elsif three && seven
-    return 'PlingPlong'
-  elsif five && seven
-    return 'PlangPlong'
-  elsif three
-    return 'Pling'
-  elsif five
-    return 'Plang'
-  elsif seven
-    return 'Plong'
-  else
-    return number.to_s
+class Raindrops
+  def self.convert(number)
+    result = ''
+    result += 'Pling' if (number % 3).zero?
+    result += 'Plang' if (number % 5).zero?
+    result += 'Plong' if (number % 7).zero?
+    result += number.to_s if result.length.zero?
+    result
   end
 end
+
+
+# def self.convert(number)
+#   seven = (number % 7).zero?
+#   five = (number % 5).zero?
+#   three = (number % 3).zero?
+#
+#   if seven && five && three
+#     return 'PlingPlangPlong'
+#   elsif five && three
+#     return 'PlingPlang'
+#   elsif three && seven
+#     return 'PlingPlong'
+#   elsif five && seven
+#     return 'PlangPlong'
+#   elsif three
+#     return 'Pling'
+#   elsif five
+#     return 'Plang'
+#   elsif seven
+#     return 'Plong'
+#   else
+#     return number.to_s
+#   end
+# end
