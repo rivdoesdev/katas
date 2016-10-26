c6918a71ad83a72ca9b7ae1d0380b9ede457a732
Kate Walters <kawalters01@gmail.com>
2016-10-26 13:33:12 -0500

    Update roman_numerals.rb
    Added NUMERALS

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,5 +1,16 @@
 class Fixnum
-  def to_roman
-    
+  NUMERALS = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+  }
+
+  def to_roman(n)
+    n = ''
+   
   end
 end
