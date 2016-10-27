47ac0f101cda2518a9b099083c820d7bb8099521
Kate Walters <kawalters01@gmail.com>
2016-10-27 16:32:42 -0500

    Update roman_numerals.rb
    Adding numerals hash back in

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,4 +1,16 @@
 class Fixnum
+    def numerals
+    {
+      1000 => 'M',
+      500 => 'D',
+      100 => 'C',
+      50 => 'L',
+      10 => 'X',
+      5 => 'V',
+      1 => 'I'
+    }
+  end
+  
   def to_roman
     'I'
   end
