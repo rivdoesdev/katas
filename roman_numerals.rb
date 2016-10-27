382fcafe95ae3eb7a07bfaef3a9edc518ef3d61e
Kate Walters <kawalters01@gmail.com>
2016-10-27 16:36:24 -0500

    Update roman_numerals.rb
    First three tests pass

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,5 +1,18 @@
 class Fixnum
-    def numerals
+  def to_roman
+    string = self
+    num = ''
+
+    numerals.each do |int, roman|
+      while string >= int
+        num << roman
+        string -= int
+      end
+    end
+    return num
+  end
+
+  def numerals
     {
       1000 => 'M',
       500 => 'D',
@@ -9,9 +22,5 @@ class Fixnum
       5 => 'V',
       1 => 'I'
     }
-  end
-  
-  def to_roman
-    'I'
   end
 end
