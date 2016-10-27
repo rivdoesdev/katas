b2d48488d897398300061545921485e4d2db1896
Kate Walters <kawalters01@gmail.com>
2016-10-27 16:22:31 -0500

    Update roman_numerals.rb
    Starting over. First test passes.

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,16 +1,5 @@
 class Fixnum
-  NUMERALS = {
-    1 => 'I',
-    5 => 'V',
-    10 => 'X',
-    50 => 'L',
-    100 => 'C',
-    500 => 'D',
-    1000 => 'M'
-  }
-
-  def to_roman(n)
-    n = ''
-   
+  def to_roman
+    'I'
   end
 end
