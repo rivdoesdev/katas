f2fd8e1117c936f33585847179daaf898ced0531
Kate Walters <kawalters01@gmail.com>
2016-10-28 18:53:29 -0500

    Update stringy.rb
    Works with any given number

--- stringy.rb
+++ stringy.rb
@@ -2,16 +2,16 @@
 #alternating '1s' and '0s'.
 
 def stringy(size)
-  if size == 0
-    return '0'
-  elsif size == 1
-    return '01'
-  elsif size == 2
-    return '010'
+  result = []
+  num = 1
 
+  size.times do |n|
+    result << num
+    num == 0 ? num = 1 : num = 0
   end
+  result.join
 end
 
-(10).times do |size|
+10.times do |size|
   puts stringy(size)
 end
