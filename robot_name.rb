1b44e216530cf1e91010aaff70c46da8a9fe64b8
Kate Walters <kawalters01@gmail.com>
2016-10-28 19:23:56 -0500

    Update robot_name.rb
    Passes all tests except uniqueness

--- robot_name.rb
+++ robot_name.rb
@@ -1,4 +1,21 @@
 class Robot
   def name
+    @name ||= generate_name
+  end
+
+  def reset
+    @name = nil
+  end
+
+  def generate_name
+    (random_letters + random_digits)
+  end
+
+  def random_letters
+    [*('A'..'Z')].sample(2).join
+  end
+
+  def random_digits
+    rand.to_s[2..4]
   end
 end
